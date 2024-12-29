d7961823844f7fed49480d7d8c79d7f2a736e2dd
岡本紘治 <>
2024-12-29 13:20:21 +0900

    radix_sort関数を25行以内に修正

--- radix_sort.c
+++ radix_sort.c
@@ -6,7 +6,7 @@
 /*   By: kokamoto <kokamoto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/29 13:05:01 by kokamoto          #+#    #+#             */
-/*   Updated: 2024/12/29 13:09:33 by kokamoto         ###   ########.fr       */
+/*   Updated: 2024/12/29 13:19:08 by kokamoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,26 @@ int    get_min(t_list *stack)
     return (min);
 }
 
+static int    get_max(t_list *stack)
+{
+    t_list  *current;
+    int     max_num;
+    int     max_bits;
+
+    current = stack;
+    max_num = *(int *)current->content;
+    while (current)
+    {
+        if (*(int *)current->content > max_num)
+            max_num = *(int *)current->content;
+        current = current->next;
+    }
+    max_bits = 0;
+    while ((max_num >> max_bits) != 0)
+        max_bits++;
+    return (max_bits);
+}
+
 void normalize_stack(t_list **stack)
 {
     t_list  *current;
@@ -46,54 +66,28 @@ void normalize_stack(t_list **stack)
     }
 }
 
-// 基数ソートのメイン関数
 void    radix_sort(t_list **a, t_list **b)
 {
     int     max_bits;
+    int     size;
     int     i;
     int     j;
-    int     size;
-    t_list  *current;
-    int     max_num;
 
     size = ft_lstsize(*a);
-    if (size <= 3)
-    {
-        sort_three(a);
-        return;
-    }
-
-    // スタックを正規化
     normalize_stack(a);
-
-    // 最大ビット数を計算
-    current = *a;
-    max_num = *(int *)current->content;
-    while (current)
+    max_bits = get_max_bits(*a);
+    i = -1;
+    while (++i < max_bits)
     {
-        if (*(int *)current->content > max_num)
-            max_num = *(int *)current->content;
-        current = current->next;
-    }
-    max_bits = 0;
-    while ((max_num >> max_bits) != 0)
-        max_bits++;
-
-    // 基数ソート
-    i = 0;
-    while (i < max_bits)
-    {
-        j = 0;
-        while (j < size)
+        j = -1;
+        while (++j < size)
         {
             if ((*(int *)(*a)->content >> i) & 1)
                 ra(a);
             else
                 pb(a, b);
-            j++;
         }
         while (*b)
             pa(a, b);
-        i++;
     }
 }
